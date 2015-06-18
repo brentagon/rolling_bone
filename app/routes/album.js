68c1689087f9cf8d062e0f8619177f10ca60cd2d
Guest User <student@epicodus.com>
2015-06-18 14:52:50 -0700

    Added review functionality.

--- app/routes/album.js
+++ app/routes/album.js
@@ -2,19 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var albumJSONandReviews;
     var controller = this;
     var url_albums = 'https://api.discogs.com/database/search?release_id=' + params.album_id + '&token=FlOvGcZADMoHMGuNNhOsyPSqiLIZBqZQZbatvtML';
     return Ember.$.getJSON(url_albums).then(function(responseJSON) {
-      return albumJSONandReviews = {
-        JSON: responseJSON.results[0],
-        reviews: controller.store.find('album', params.album_id).then(function(foundAlbum) {
-          return foundAlbum.get('reviews');
-        })
-      };
-    }).then(function() {
-      debugger;
-    return albumJSONandReviews;
-  });
+      var JSON = responseJSON.results[0];
+      return controller.store.find('album').then(function(allAlbums) {
+        var foundAlbum;
+        allAlbums.forEach(function(individualAlbum) {
+          if(individualAlbum.get('discogsId') === params.album_id) {
+            foundAlbum = individualAlbum;
+          }
+        });
+        return {
+          JSON: JSON,
+          reviews: foundAlbum.get('reviews')
+        };
+      });
+    });
   }
 });
